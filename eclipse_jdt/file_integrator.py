68a3886af35a4e31c11e6fec87bd3dde8b636c40
hideshis <hideshis1990@gmail.com>
2015-06-11 20:55:13 +0900

    modified the scripts for plotting change history view. If the file doesn't have corresponding test/product code, then skip to the next loop.

--- eclipse_jdt/file_integrator.py
+++ eclipse_jdt/file_integrator.py
@@ -23,10 +23,20 @@ def id_getter(file_name):
 	id = id.replace("\n", "")
 	return id
 
+def checker(id):
+	command = "grep \"," + id + "$\" file_id_list.csv"
+	result = subprocess.check_output(command)
+	result_list = result.split("\n")
+	if len(result_list) == 3:
+		flag = 1
+	else:
+		flag = 0
+	return flag
+
 def file_integrator(pjt_name):
 	f = open(pjt_name+".csv", "rb")
 	writer = csv.writer(file("change_history_view.csv", "w"), lineterminator="\n")
-	header = ["Data", "Author", "File", "Attribution", "ID"]
+	header = ["Date", "Author", "File", "Attribution", "ID"]
 	writer.writerow(header)
 	dataReader = csv.reader(f)
 	flag = 0
@@ -39,9 +49,16 @@ def file_integrator(pjt_name):
 		file_name = data[2]
 		attribution = data[3]
 		id = id_getter(file_name)
+		check_flag = checker(id)
+		# if the file doesn't have corresponding test/product code, then skip to the next loop.
+		if check_flag == 0:
+			print str(flag) + " / 86553 have been finished."
+			flag += 1
+			continue
 		file_info = []
 		file_info.append(date)
 		file_info.append(author)
+		file_info.append(file_name)
 		file_info.append(attribution)
 		file_info.append(id)
 		writer.writerow(file_info)
