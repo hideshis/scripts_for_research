6c3dac277ff386340b698b90116d3cf1cf424b0b
hideshis <hideshis1990@gmail.com>
2015-12-04 12:43:56 +0900

    Modified scripts for analyzing sonarQube.

--- IWESEP2016/sonarQube/hoge.py
+++ IWESEP2016/sonarQube/hoge.py
@@ -7,6 +7,8 @@ import os
 import datetime
 import subprocess
 
+def sonar_analysis():
+
 def git_clone_and_checkouter(commit_hash):
 	os.system('rm -Rf target')
 	os.system('git clone https://github.com/apache/httpclient.git target')
@@ -41,13 +43,43 @@ def executable_tc_list_getter(tc_list):
 	for file in fild_all_files('./target'):
 		file = file.replace('./target/', '')
 		file_list.append(file)
-	print len(tc_list)
 	tc_list = tc_list + file_list
 	s = set()
 	executable_tc_list = [x for x in tc_list if x in s or s.add(x)]
 	return executable_tc_list
 
+def test_sonar_execution():
+	os.chdir('./target')
+	os.system('mvn clean org.jacoco:jacoco-maven-plugin:0.7.4.201502262128:prepare-agent install -Dmaven.test.failure.ignore=true')
+	os.system('mvn sonar:sonar > sonar_log.txt')
+	os.system('mv sonar_log.txt ..')
+	os.chdir('..')
+	return
 
+def execution_result_checker():
+	query = '\[INFO\] BUILD SUCCESS'
+	try:
+		result = subprocess.check_output('grep "' + query + '" sonar_log.txt', shell=True)
+		return "success"
+	except subprocess.CalledProcessError:
+	 	return "failure"
+	sys.exit()
+	return
+
+commit_hash_list = commit_hash_getter()
+for commit_hash in commit_hash_list:
+	git_clone_and_checkouter(commit_hash)
+	test_sonar_execution()
+	execution_result = execution_result_checker()
+	if execution_result == "failure":
+		continue
+	else:
+		print commit_hash
+		print execution_result
+		soanr_analysis()
+	sys.exit()
+
+"""
 pattern_list = os.listdir('./')
 for pattern in pattern_list:
 	if not pattern.endswith(".csv"):
@@ -57,3 +89,17 @@ for pattern in pattern_list:
 	for commit_hash in commit_hash_list:
 		git_clone_and_checkouter(commit_hash)
 		executable_tc_list = executable_tc_list_getter(tc_list)
+		if len(executable_tc_list) == 0:
+			continue
+		test_sonar_execution(executable_tc_list)
+		execution_result = execution_result_checker()
+		print execution_result
+		if execution_result == "failure":
+			continue
+		else:
+			print pattern
+			print commit_hash
+			print executable_tc_list
+			print execution_result
+		sys.exit()
+"""
