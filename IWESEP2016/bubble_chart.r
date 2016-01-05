a439b19aa565905160ef7fc3f443bd9f7f66758f
hideshis <hideshis1990@gmail.com>
2016-01-05 09:22:30 +0900

    updated the script for bubble chart.

--- IWESEP2016/bubble_chart.r
+++ IWESEP2016/bubble_chart.r
@@ -3,3 +3,5 @@ dead <- subset(hoge, status == "dead")
 arrive <- subset(hoge, status == "arrive")
 library(ggplot2)
 ggplot(data=hoge, aes(x=hoge$lifetime, y=hoge$co.evolution.rate, colour=hoge$status), xlab("lifetime")) + geom_point(aes(size=hoge$average.bug, alpha=.5)) + scale_size_continuous(range = c(2, 10)) + labs(size="size", x="lifetime", y="degree of co-evolution", alpha="alpha", colour="status")
+
+ggplot(data=hoge, aes(x=hoge$lifetime, y=hoge$co.evolution.rate, colour=hoge$status), xlab("lifetime")) + geom_point(aes(size=hoge$average.bug, alpha=.5)) + scale_size_continuous(range = c(2, 10)) + labs(size="size", x="lifetime", y="degree of co-evolution", alpha="alpha", colour="status") + geom_vline(xintercept = median(hoge$lifetime)) + geom_hline(yintercept = median(hoge$co.evolution.rate))
