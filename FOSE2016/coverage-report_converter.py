b36571ff927a20fda37b8afd61781d89ad15a48a
hideshis <hideshis1990@gmail.com>
2016-05-04 01:19:08 +0900

    Revised coverage-report_conver.py. Each of file-names created by the script was too long... So the new script creates directories based on the HTML-file's file path instead of binding whole paths and use it as CSV-file name.

--- FOSE2016/coverage-report_converter.py
+++ FOSE2016/coverage-report_converter.py
@@ -11,36 +11,58 @@ def fild_all_files(directory):
         for file in files:
             yield os.path.join(root, file)
 
-os.system('rm -rf ./coverage-report_formatted')
-os.system('mkdir coverage-report_formatted')
-for file_name in fild_all_files('./coverage-report'):
-    if file_name.endswith('.html') and (file_name != './coverage-report/index.html'):
-        print file_name
-        file_name_output = file_name[2:]
-        file_name_output = file_name_output.replace('/', '_')
-        file_name_output = file_name_output.replace('.html', '.csv')
-        f_write = open('./coverage-report_formatted/'+file_name_output, 'w')
-        csvWriter = csv.writer(f_write, lineterminator='\n')
-        f = open(file_name, 'r')
-        root = lh.fromstring(f.read().decode('utf-8'))
-        tr_list = root.xpath('/html/body/table/tr')
-        for tr in tr_list:
-            cover_list = []
-            for td in tr:
-                try:
-                    if td.attrib['class'] == 'line':
-                        #print 'line No.: ' + str(td.text)
-                        lineNo = str(td.text)
-                except KeyError:
-                    if (td.find('pre') != None) and (td.find('pre').attrib['class'] == 'prettyprint covered cp'):
-                        ol = td.find('ol')
-                        cover_list.append(lineNo)
-                        for li in ol:
-                            #print li.text
-                            cover_list.append(li.text.replace(': ', '#'))
-            if len(cover_list) > 0:
-                csvWriter.writerow(cover_list)
-                print ','.join(cover_list)
-                print '---'
-        f.close()
-        f_write.close()
+def directory_creator(file_path, file_name):
+    if not os.path.isdir(file_path):
+        os.makedirs(file_path)
+    return
+
+def html_convertor(file_path, file_name, file_name_original):
+    file_name_output = file_name.replace('.html', '.csv')
+    f_write = open(file_path + '/' + file_name_output, 'w')
+    csvWriter = csv.writer(f_write, lineterminator='\n')
+    f = open(file_name_original, 'r')
+    root = lh.fromstring(f.read().decode('utf-8'))
+    tr_list = root.xpath('/html/body/table/tr')
+    # one tr equals to one line of code
+    for tr in tr_list:
+        cover_list = []
+        # Each of tds contains detail information of one line of code
+        for td in tr:
+            try:
+                # get line number
+                if td.attrib['class'] == 'line':
+                    lineNo = str(td.text)
+            except KeyError:
+                # get test cases which tests the line of code
+                if (td.find('pre') != None) and (td.find('pre').attrib['class'] == 'prettyprint covered cp'):
+                    ol = td.find('ol')
+                    cover_list.append(lineNo)
+                    # Each of lis contains name of test case.
+                    for li in ol:
+                        cover_list.append(li.text.replace(': ', '#'))
+        # if len(cover_list) > 0 is True, it means that one or more test cases
+        # cover the line of code.
+        if len(cover_list) > 0:
+            csvWriter.writerow(cover_list)
+            print ','.join(cover_list)
+            print '---'
+    f.close()
+    f_write.close()
+    return
+
+def file_name_and_path_getter(file_name):
+    file_name = file_name[2:]
+    file_name = file_name.replace('/target/coverage-report/', '/')
+    path_list = file_name.split('/')
+    file_name = path_list.pop(-1)
+    file_path = './coverage-report_converted/' + '/'.join(path_list)
+    return file_path, file_name
+
+os.system('rm -rf ./coverage-report_converted')
+for file_name in fild_all_files('./'):
+    if ('/coverage-report/' in file_name) and file_name.endswith('.html') and (not file_name.endswith('/coverage-report/index.html')):
+        file_name_original = file_name
+        file_path, file_name = file_name_and_path_getter(file_name)
+        print file_path, file_name
+        directory_creator(file_path, file_name)
+        html_convertor(file_path, file_name, file_name_original)
