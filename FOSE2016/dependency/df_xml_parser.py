a4e21e81c043f87f5de42042b1a9a853243bab10
hideshis <hideshis1990@gmail.com>
2016-05-25 21:36:24 +0900

    Modified df_xml_parser.py. The newest version formats the parameter types of methods. Example: hoge(java.util.String, int) -> hoge(String, int)

--- FOSE2016/dependency/df_xml_parser.py
+++ FOSE2016/dependency/df_xml_parser.py
@@ -20,10 +20,25 @@ for e_package in root.findall('package'):
             e_class_name = 'no_class_name'
         #print e_package_name + ',' + e_class_name
         for e_feature in e_class.findall('feature'):
-            e_feature_name = e_feature.find('name').text
-            if e_feature_name == None:
+            tmp = e_feature.find('name').text
+            print e_package_name + ',' + e_class_name + ',' + tmp
+            if tmp == None:
                 e_feature_name = 'no_feature_name'
-            print e_package_name + ',' + e_class_name + ',' + e_feature_name
+            else:
+                try:
+                    e_feature_name = tmp[:tmp.index("(")] # hoge(foo.var.fuga, piyo) -> hoge
+                    e_feature_parameter = tmp[tmp.index("(")+1:-1] # hoge(foo.var.fuga, piyo) -> foo.var.fuga, piyo
+                    if e_feature_parameter != '()':
+                        e_feature_parameter_list = []
+                        for param in e_feature_parameter.split(','): # foo.var.fuga, piyo -> [fuga, piyo]
+                            param = param.replace(' ', '')
+                            param = param.split('.')[-1]
+                            e_feature_parameter_list.append(param)
+                        e_feature_name = e_feature_name + '(' + ','.join(e_feature_parameter_list) + ')'
+                    else:
+                        e_feature_name = e_feature_name + '()'
+                except ValueError:
+                    e_feature_name = tmp
             info = [e_package_name, e_class_name, e_feature_name]
             csvWriter.writerow(info)
 f.close()
