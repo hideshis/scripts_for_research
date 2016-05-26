3d2df5524b52b6ea6c0f8e043c4c2e6ab6839e5a
hideshis <hideshis1990@gmail.com>
2016-05-26 18:05:56 +0900

    Updated pom_edit1.sh. We have refactored the script and then its readability has become higher!

--- FOSE2016/pom_edit/pom_edit1.sh
+++ FOSE2016/pom_edit/pom_edit1.sh
@@ -21,10 +21,17 @@ else
 	echo $result
 	arr=(`echo $result | tr -s ':' ' '`)
 	lineNo=${arr[0]}
-	insert $pom $lineNo "<properties>"
-	let lineNo+=1
-	insert $pom $lineNo "<jmockit.version>1.23</jmockit.version>"
-	let lineNo+=1
-	insert $pom $lineNo "</properties>"
+	flag=0
+	script_array=("<properties>" "<jmockit.version>1.23</jmockit.version>" "</properties>")
+	for script in ${script_array[@]}
+	do
+		if [ $flag = 0 ];
+		then
+			let flag+=1
+		else
+			let lineNo+=1
+		fi
+		insert $pom $lineNo $script
+	done
 fi
 #result=`grep -n '</dependencyManagement>' $1`
