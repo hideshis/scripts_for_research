3901aecb387a2a7e68ab5bdecc9accf2a94d0ff5
hideshis <hideshis1990@gmail.com>
2016-06-06 21:29:46 +0900

    Class Diagram to make scripting easier. Also, I added functions that identifies outbound dependency to df_xml_parer.py.

--- FOSE2016/dependency/df_xml_parser.py
+++ FOSE2016/dependency/df_xml_parser.py
@@ -5,6 +5,28 @@ import csv
 import subprocess
 import os
 
+def name_conversion(target):
+    if target == None:
+        return 'no_name'
+    else:
+        try:
+            target_name = target[:target.index("(")] # hoge(foo.var.fuga, piyo) -> hoge
+            target_parameter = target[target.index("(")+1:-1] # hoge(foo.var.fuga, piyo) -> foo.var.fuga, piyo
+            if target_parameter != '()':
+                target_parameter_list = []
+                for param in target_parameter.split(','): # foo.var.fuga, piyo -> [fuga, piyo]
+                    param = param.replace(' ', '')
+                    param = param.split('.')[-1]
+                    param = param.split('$')[-1]
+                    target_parameter_list.append(param)
+                    target_name = target_name + '(' + ','.join(target_parameter_list) + ')'
+                    return target_name
+            else:
+                return target_name + '()'
+        except ValueError:
+            return target
+
+
 argvs = sys.argv
 xml_file = argvs[1]
 output_file = argvs[2]
@@ -23,7 +45,9 @@ for e_package in root.findall('package'):
             e_class_name = 'no_class_name'
         #print e_package_name + ',' + e_class_name
         for e_feature in e_class.findall('feature'):
-            tmp = e_feature.find('name').text
+            #tmp = e_feature.find('name').text
+            e_feature_name = name_conversion(e_feature.find('name').text)
+            """
             if tmp == None:
                 e_feature_name = 'no_feature_name'
             else:
@@ -42,6 +66,16 @@ for e_package in root.findall('package'):
                         e_feature_name = e_feature_name + '()'
                 except ValueError:
                     e_feature_name = tmp
-            info = [e_package_name, e_class_name, e_feature_name]
-            csvWriter.writerow(info)
+                """
+            outbound_list = []
+            for outbound in e_feature.findall('outbound'):
+                outbound_list.append(name_conversion(outbound.text))
+            #os.system('echo "' + str(len(outbound_list)) + '">>hogehoge.txt')
+            if len(outbound_list) == 0:
+                info = [e_package_name, e_class_name, e_feature_name, ""]
+                csvWriter.writerow(info)
+            else:
+                for outbound in outbound_list:
+                    info = [e_package_name, e_class_name, e_feature_name, outbound]
+                    csvWriter.writerow(info)
 f.close()
