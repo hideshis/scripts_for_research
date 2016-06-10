0d54407832d793b928623bc4acba55dacdf64f18
hideshis <hideshis1990@gmail.com>
2016-06-10 11:48:06 +0900

    added commitHash() method which output all hash commits of all degignated repository.

--- FOSE2016/tag_commitHash_link/tag_commitHash_link.py
+++ FOSE2016/tag_commitHash_link/tag_commitHash_link.py
@@ -4,13 +4,30 @@ import subprocess
 import os
 import csv
 
+def commitHash(git_repo_path):
+    output_file_path = '/Users/hideshi-s/Desktop/scripts_for_research/FOSE2016/tag_commitHash_link/commitHash.csv'
+    os.system('rm ' + output_file_path)
+    os.chdir(git_repo_path)
+    result = subprocess.check_output('git log --pretty=format:%\H', shell=True)
+    result = result.replace('\r', '')
+    result = result.replace('refs/tags/', '')
+    result = result.replace(' ', ',')
+    for line in result.split('\n'):
+        os.system('echo ' + line + '>> ' + output_file_path)
+    return
+
+def tag_commitHash_link(git_repo_path):
+    output_file_path = '/Users/hideshi-s/Desktop/scripts_for_research/FOSE2016/tag_commitHash_link/tag_commitHash_link.csv'
+    os.system('rm ' + output_file_path)
+    os.chdir(git_repo_path)
+    result = subprocess.check_output('git show-ref --tags | grep "_BUG"', shell=True)
+    result = result.replace('\r', '')
+    result = result.replace('refs/tags/', '')
+    result = result.replace(' ', ',')
+    for line in result.split('\n'):
+        os.system('echo ' + line + '>> ' + output_file_path)
+    return
+
 git_repo_path = '/Users/hideshi-s/Desktop/exp/result/15/0626seminar2/httpclient/original.git'
-output_file_path = '/Users/hideshi-s/Desktop/scripts_for_research/FOSE2016/tag_commitHash_link/tag_commitHash_link.csv'
-os.system('rm ' + output_file_path)
-os.chdir(git_repo_path)
-result = subprocess.check_output('git show-ref --tags | grep "_BUG"', shell=True)
-result = result.replace('\r', '')
-result = result.replace('refs/tags/', '')
-result = result.replace(' ', ',')
-for line in result.split('\n'):
-    os.system('echo ' + line + '>> ' + output_file_path)
+tag_commitHash_link(git_repo_path)
+commitHash(git_repo_path)
