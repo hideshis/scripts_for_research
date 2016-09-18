713150ee7eea3bbb25ad81003418d17bb4a400fd
hideshis <hideshis1990@gmail.com>
2016-09-18 23:58:31 +0900

    updated python script.

--- MSRMiningChallenge2017/git_url_getter.py
+++ MSRMiningChallenge2017/git_url_getter.py
@@ -2,10 +2,18 @@
 import os
 
 f = open('maven_projects.csv', 'r')
-pjt_name = f.readline().replace('\r', '').replace('\n', '')
-while pjt_name:
-    git_url = 'https://github.com/' + pjt_name + '.git'
+pjt_id = f.readline().replace('\r', '').replace('\n', '')
+pjt_name = pjt_id.split('/')[-1]
+while pjt_id:
+    git_url = 'https://github.com/' + pjt_id + '.git'
     print git_url
-    #os.system('git clone ' + git_url)
-    pjt_name = f.readline().replace('\r', '').replace('\n', '')
+    os.system('git clone ' + git_url)
+    os.chdir('./pom_edit/jacoco')
+    os.system('python pom_editor_exe.py ' + pjt_name)
+    os.chdir('../..')
+    os.chdir('./' + pjt_name)
+    os.system('mvn clean test jacoco:report -fn')
+    os.chdir('..')
+    pjt_id = f.readline().replace('\r', '').replace('\n', '')
+    pjt_name = pjt_id.split('/')[-1]
 f.close()
