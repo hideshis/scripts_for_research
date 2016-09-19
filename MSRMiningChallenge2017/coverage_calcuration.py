7ab5f804937a7d8205002fab8823b6790b48de6b
kenjiro <kenjiro@isb3-dhcp-172-238.naist.jp>
2016-09-20 00:00:31 +0900

    updated coverage_calcuration.py and added its result. I needed to catch some exceptions during the calcuration and then updated it.

--- MSRMiningChallenge2017/coverage_calcuration.py
+++ MSRMiningChallenge2017/coverage_calcuration.py
@@ -7,6 +7,7 @@ import sys
 import os
 import urllib2
 import lxml.html
+import csv
 from lxml import etree
 
 f_write = open('coverage_info.csv', 'w')
@@ -14,28 +15,44 @@ csvWriter = csv.writer(f_write, lineterminator='\n')
 f_read = open('maven_projects.csv', 'r')
 pjt_id = f_read.readline().replace('\r', '').replace('\n', '')
 pjt_name = pjt_id.split('/')[-1]
-pjt_path = '/Users/hideshi-s/Desktop/msrmc2017/' + pjt_name
+pjt_path = '/Users/kenjiro/Desktop/hideshi-s/msrmc2017/' + pjt_name
 loc_list = []
 uncovered_loc_list = []
 while pjt_name:
-    result = subprocess.check_output('find ' + pjt_path + ' | grep "jacoco/index\.html$"', shell=True)
-    result = result.replace('\r', '')
-    result_list = result.split('\n')
-    result_list.pop()
-    for html_path in result_list:
-        print '_' + html_path + '_'
-        root = lxml.html.parse(html_path).getroot()
-        uncovered_loc_list.append(int(root.xpath('//*[@id="coveragetable"]/tfoot/tr/td[8]')[0].text.replace(',','')))
-        loc_list.append(int(root.xpath('//*[@id="coveragetable"]/tfoot/tr/td[9]')[0].text.replace(',','')))
-    print pjt_id + '->line coverage: ' + str(100 * (float(sum(loc_list) - sum(uncovered_loc_list)) / sum(loc_list))) + '%'
-    coverage_info = []
-    coverage_info.append(pjt_id)
-    coverage_info.append(100 * (float(sum(loc_list) - sum(uncovered_loc_list)) / sum(loc_list)))
-    csvWriter.writerow(coverage_info)
-    pjt_id = f_read.readline().replace('\r', '').replace('\n', '')
-    pjt_name = pjt_id.split('/')[-1]
-    pjt_path = '/Users/hideshi-s/Desktop/msrmc2017/' + pjt_name
-    loc_list = []
-    uncovered_loc_list = []
+	try:
+		result = subprocess.check_output('find ' + pjt_path + ' | grep "jacoco/index\.html$"', shell=True)
+		coverage_report_existence_flag = True
+	except subprocess.CalledProcessError:
+		coverage_report_existence_flag = False
+	if coverage_report_existence_flag == True:
+		result = result.replace('\r', '')
+		result_list = result.split('\n')
+		result_list.pop()
+		for html_path in result_list:
+			print '_' + html_path + '_'
+			root = lxml.html.parse(html_path).getroot()
+			try:
+				uncovered_loc_list.append(int(root.xpath('//*[@id="coveragetable"]/tfoot/tr/td[8]')[0].text.replace(',','')))
+				loc_list.append(int(root.xpath('//*[@id="coveragetable"]/tfoot/tr/td[9]')[0].text.replace(',','')))
+			except IndexError:
+				uncovered_loc_list.append(0)
+				loc_list.append(0)
+		coverage_info = []
+		coverage_info.append(pjt_id)
+		try:
+			print pjt_id + '->line coverage: ' + str(100 * (float(sum(loc_list) - sum(uncovered_loc_list)) / sum(loc_list))) + '%'
+			coverage_info.append(100 * (float(sum(loc_list) - sum(uncovered_loc_list)) / sum(loc_list)))
+		except ZeroDivisionError:
+			coverage_info.append('N/A')
+	else:
+		coverage_info = []
+		coverage_info.append(pjt_id)
+		coverage_info.append('N/A')
+	csvWriter.writerow(coverage_info)
+	pjt_id = f_read.readline().replace('\r', '').replace('\n', '')
+	pjt_name = pjt_id.split('/')[-1]
+	pjt_path = '/Users/kenjiro/Desktop/hideshi-s/msrmc2017/' + pjt_name
+	loc_list = []
+	uncovered_loc_list = []
 f_write.close()
 f_read.close()
