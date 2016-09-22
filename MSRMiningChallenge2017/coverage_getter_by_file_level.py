38794ae7e0282ce6e50a4e5d8bd58da6937b1650
hideshis <hideshis1990@gmail.com>
2016-09-22 18:05:55 +0900

    added a script for extracting C0 coverage for each method in projects.

---
+++ MSRMiningChallenge2017/coverage_getter_by_file_level.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+This script parses index.html files created by Jacoco and obtain test coverage for each file.
+"""
+import subprocess
+import sys
+import os
+import urllib2
+import lxml.html
+import csv
+from lxml import etree
+
+f_write = open('coverage_info_by_file_level.csv', 'w')
+csvWriter = csv.writer(f_write, lineterminator='\n')
+header = ['project', 'num_covered_instruction', 'num_uncovered_instruction', 'C0coverage']
+csvWriter.writerow(header)
+f_read = open('coverage_info.csv', 'r')
+pjt_id = f_read.readline().split(',')[0]
+coverage_of_pjt = f_read.readline().split(',')[-1].replace('\r','').replace('\n','')
+pjt_name = pjt_id.split('/')[-1]
+pjt_path = '/Users/kenjiro/Desktop/hideshi-s/msrmc2017/' + pjt_name
+#pjt_path = '/Users/hideshi-s/Desktop/msrmc2017/' + pjt_name
+loc_list = []
+uncovered_loc_list = []
+while pjt_name:
+	if coverage_of_pjt == 'N/A':
+		pjt_id = f_read.readline().split(',')[0]
+		coverage_of_pjt = f_read.readline().split(',')[-1].replace('\r','').replace('\n','')
+		pjt_name = pjt_id.split('/')[-1]
+		pjt_path = '/Users/kenjiro/Desktop/hideshi-s/msrmc2017/' + pjt_name
+		#pjt_path = '/Users/hideshi-s/Desktop/msrmc2017/' + pjt_name
+        continue
+	try:
+		result = subprocess.check_output('find ' + pjt_path + ' | grep "/target/site/jacoco/" | grep "\.html$" | grep -v "\.java\.html$" | grep -v "index\.html$" | grep -v "index\.source\.html$" | grep -v "jacoco-sessions\.html" | grep -v "\.sessions\.html"', shell=True)
+		coverage_report_existence_flag = True
+	except subprocess.CalledProcessError:
+		coverage_report_existence_flag = False
+	if coverage_report_existence_flag == True:
+		result = result.replace('\r', '')
+		result_list = result.split('\n')
+		result_list.pop()
+		#for html_path in result_list:
+		#	print html_path
+		for html_path in result_list:
+			print html_path
+			root = lxml.html.parse(html_path).getroot()
+			for tr_counter in range(1, len(root.xpath("/html/body/table/tbody/tr"))+1):
+				C0coverage = root.xpath("/html/body/table/tbody/tr[" + str(tr_counter) + "]/td[3]")[0].text
+				print C0coverage
+				if C0coverage == '0%':
+					num_covered_instruction = 0
+					try:
+						num_uncovered_instruction = int(root.xpath("/html/body/table/tbody/tr[" + str(tr_counter) + "]/td[2]/img")[0].attrib['alt'].replace(',',''))
+					except IndexError:
+						num_uncovered_instruction = 'N/A'
+				elif C0coverage == '100%':
+					try:
+						num_covered_instruction = int(root.xpath("/html/body/table/tbody/tr[" + str(tr_counter) + "]/td[2]/img")[0].attrib['alt'].replace(',',''))
+					except IndexError:
+						num_covered_instruction = 'N/A'
+					num_uncovered_instruction = 0
+				else:
+					for img_counter in range(1,3):
+						try:
+							if "greenbar.gif" in root.xpath("/html/body/table/tbody/tr[" + str(tr_counter) + "]/td[2]/img["+str(img_counter)+"]")[0].attrib['src']:
+								num_covered_instruction = int(root.xpath("/html/body/table/tbody/tr[" + str(tr_counter) + "]/td[2]/img["+str(img_counter)+"]")[0].attrib['alt'].replace(',',''))
+							else:
+								num_uncovered_instruction = int(root.xpath("/html/body/table/tbody/tr[" + str(tr_counter) + "]/td[2]/img["+str(img_counter)+"]")[0].attrib['alt'].replace(',',''))
+						except IndexError:
+							num_covered_instruction = 'N/A'
+							num_uncovered_instruction = 'N/A'
+					print num_covered_instruction, num_uncovered_instruction
+				print num_covered_instruction, num_uncovered_instruction
+				coverage_info = []
+				coverage_info.append(pjt_id)
+				coverage_info.append(num_covered_instruction)
+				coverage_info.append(num_uncovered_instruction)
+				coverage_info.append(C0coverage)
+				csvWriter.writerow(coverage_info)
+	else:
+		coverage_info = []
+		coverage_info.append(pjt_id)
+		coverage_info.append('N/A')
+		coverage_info.append('N/A')
+		coverage_info.append('N/A')
+		csvWriter.writerow(coverage_info)
+	pjt_id = f_read.readline().split(',')[0]
+	coverage_of_pjt = f_read.readline().split(',')[-1].replace('\r','').replace('\n','')
+	pjt_name = pjt_id.split('/')[-1]
+	pjt_path = '/Users/kenjiro/Desktop/hideshi-s/msrmc2017/' + pjt_name
+	#pjt_path = '/Users/hideshi-s/Desktop/msrmc2017/' + pjt_name
+f_write.close()
+f_read.close()
